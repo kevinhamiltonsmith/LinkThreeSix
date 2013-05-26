0d72e722ccc6de55d7845c67017fa394ae597894
Hack Reactor Students <github@hackreactor.com>
2013-05-26 15:20:22 -0700

    css styling of app and game board

--- app/views/BoardView.js
+++ app/views/BoardView.js
@@ -27,13 +27,13 @@ var BoardView = Backbone.View.extend({
   render: function(){
     var board = this.model.get('board');
     var html ="<tr class='row-a'>" +
-                "<td data-position='0' class='a0'>" + board[0] + "</td><td data-position='1' class='a1'>" + board[1] + "</td><td data-position='2' class='a2'>" + board[2] + "</td>" +
+                "<td data-position='0' class='a0 col0'>" + board[0] + "</td><td data-position='1' class='a1 col1'>" + board[1] + "</td><td data-position='2' class='a2 col2'>" + board[2] + "</td>" +
               "</tr>" +
               "<tr class='row-b'>" +
-                "<td data-position='3' class='b0'>" + board[3] + "</td><td data-position='4' class='b1'>" + board[4] + "</td><td data-position='5' class='b2'>" + board[5] + "</td>" +
+                "<td data-position='3' class='b0 col0'>" + board[3] + "</td><td data-position='4' class='b1 col1'>" + board[4] + "</td><td data-position='5' class='b2 col2'>" + board[5] + "</td>" +
               "</tr>" +
               "<tr class='row-c'>" +
-                "<td data-position='6' class='c0'>" + board[6] + "</td><td data-position='7' class='c1'>" + board[7] + "</td><td data-position='8' class='c2'>" + board[8] + "</td>" +
+                "<td data-position='6' class='c0 col0'>" + board[6] + "</td><td data-position='7' class='c1 col1'>" + board[7] + "</td><td data-position='8' class='c2 col2'>" + board[8] + "</td>" +
               "</tr>";
     return this.$el.html(html);
   }
