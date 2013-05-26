7a2969f6ee935aa6f33b95f7dc16a4077bc164f7
Hack Reactor Students <github@hackreactor.com>
2013-05-26 16:54:33 -0700

    board mark (X or O) now changes each time a player takes a turn

--- app/models/Board.js
+++ app/models/Board.js
@@ -1,17 +1,31 @@
 var Board = Backbone.Model.extend({
 
   initialize: function(){
-    this.set({board: [0,0,0,0,0,0,0,0,0]});
+    this.set({board: [0,0,0,0,0,0,0,0,0], turn: "player"});
   },
 
   setSquare: function(mark, position){
     //clone array here to preserve Backbone change listener in BoardView
     if (mark === 0) {
-      mark = "X";
+      if (this.get("turn") === "player") {
+        mark = "X";
+        this.computerMove();
+      } else {
+        mark = "O";
+        this.playerMove();
+      }
       var newSquares = this.get('board').slice(0);
       newSquares[position] = mark;
       this.set('board', newSquares);
     }
+  },
+
+  computerMove: function() {
+    this.set({turn: "computer"});
+  },
+
+  playerMove: function() {
+    this.set({turn: "player"});
   }
 
 });
