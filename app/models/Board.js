a598e4ecc4822c635182bb692efd9f6cad436be3
Hack Reactor Students <github@hackreactor.com>
2013-05-26 11:35:00 -0700

    built setSquare method on Board model

--- app/models/Board.js
+++ app/models/Board.js
@@ -2,6 +2,14 @@ var Board = Backbone.Model.extend({
 
   initialize: function(){
     this.set({board: [0,0,0,0,0,0,0,0,0]});
+  },
+
+  setSquare: function(mark, position){
+    //clone array here to preserve Backbone change listener in BoardView
+    var newSquares = this.get('board').slice(0);
+    newSquares[position] = mark;
+    this.set('board', newSquares);
+    console.log(this.get('board'));
   }
 
 });
--- app/views/BoardView.js
+++ app/views/BoardView.js
@@ -1,23 +1,26 @@
 var BoardView = Backbone.View.extend({
 
-  initialize: function(){
+  tagName: 'table',
 
+  className: 'board-view',
+
+  initialize: function(){
+    this.model.on('change:board', function(){
+      this.render();
+    }, this);
   },
 
   render: function(){
     var board = this.model.get('board');
-    var html ="<table>" +
-                "<tr class='row-a'>" +
-                  "<td class='a0'>" + board[0] + "</td><td class='a1'>" + board[1] + "</td><td class='a2'>" + board[2] + "</td>" +
-                "</tr>" +
-                "<tr class='row-b'>" +
-                  "<td class='b0'>" + board[3] + "</td><td class='b1'>" + board[4] + "</td><td class='b2'>" + board[5] + "</td>" +
-                "</tr>" +
-                "<tr class='row-c'>" +
-                  "<td class='c0'>" + board[6] + "</td><td class='c1'>" + board[7] + "</td><td class='c2'>" + board[8] + "</td>" +
-                "</tr>" +
-              "</table>";
-
+    var html ="<tr class='row-a'>" +
+                "<td class='a0'>" + board[0] + "</td><td class='a1'>" + board[1] + "</td><td class='a2'>" + board[2] + "</td>" +
+              "</tr>" +
+              "<tr class='row-b'>" +
+                "<td class='b0'>" + board[3] + "</td><td class='b1'>" + board[4] + "</td><td class='b2'>" + board[5] + "</td>" +
+              "</tr>" +
+              "<tr class='row-c'>" +
+                "<td class='c0'>" + board[6] + "</td><td class='c1'>" + board[7] + "</td><td class='c2'>" + board[8] + "</td>" +
+              "</tr>";
     return this.$el.html(html);
   }
 });
