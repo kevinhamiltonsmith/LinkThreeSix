5aba54d5f016f56d000bf3121241ca6d3c943cb8
Hack Reactor Students <github@hackreactor.com>
2013-05-26 17:23:09 -0700

    added Tie to scoreboard and styled

--- app/models/Game.js
+++ app/models/Game.js
@@ -1,7 +1,7 @@
 var Game = Backbone.Model.extend({
 
   initialize: function(){
-    this.set({playerScore: 0, computerScore: 0});
+    this.set({playerScore: 0, computerScore: 0, tieScore: 0});
     this.set('board', new Board());
   },
 
--- app/views/GameView.js
+++ app/views/GameView.js
@@ -10,8 +10,9 @@ var GameView = Backbone.View.extend({
     var html = "<h1>Backbone Tic-Tac-Toe</h1>" +
                   "<div class='scoreboard'>" +
                     "<h2>Wins</h2>" +
-                    "<div class='player-score'>Player: " + this.model.get('playerScore') + "</div>" +
-                    "<div class='comp-score'>Computer: " + this.model.get('computerScore') + "</div>" +
+                    "<div class='player-score'>Player 1: <span>" + this.model.get('playerScore') + "</span></div>" +
+                    "<div class='computer-score'>Player 2: <span>" + this.model.get('computerScore') + "</span></div>" +
+                    "<div class='tie-score'>Tie: <span>" + this.model.get('tieScore') + "</span></div>" +
                   "</div>";
     return this.$el.html([
       html,
