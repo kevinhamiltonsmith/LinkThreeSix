68ff54ca376cf7f7aff3d04befc1a77de55f8030
Hack Reactor Students <github@hackreactor.com>
2013-05-26 19:02:28 -0700

    added event listening to check for win each time a player makes a move

--- app/models/Game.js
+++ app/models/Game.js
@@ -3,6 +3,10 @@ var Game = Backbone.Model.extend({
   initialize: function(){
     this.set({playerScore: 0, computerScore: 0, tieScore: 0});
     this.set('board', new Board());
+
+    this.get('board').on('change', function(){
+      this.winCheck();
+    }, this);
   },
 
   winCheck: function(){
