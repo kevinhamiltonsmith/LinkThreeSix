fe07e4a6105da6f27ffbde6ce616f7a5b5bb4c3e
Hack Reactor Students <github@hackreactor.com>
2013-05-26 17:12:26 -0700

    alternate mark color for each opponent refactor table html in BoardView

--- app/models/Board.js
+++ app/models/Board.js
@@ -1,21 +1,23 @@
 var Board = Backbone.Model.extend({
 
   initialize: function(){
-    this.set({board: [0,0,0,0,0,0,0,0,0], turn: "player"});
+    this.set({board: [[0,"hidden"],[0,"hidden"],[0,"hidden"],[0,"hidden"],[0,"hidden"],[0,"hidden"],[0,"hidden"],[0,"hidden"],[0,"hidden"]], turn: "player"});
   },
 
   setSquare: function(mark, position){
     //clone array here to preserve Backbone change listener in BoardView
     if (mark === 0) {
+      var color = "playerBlue";
       if (this.get("turn") === "player") {
         mark = "X";
         this.computerMove();
       } else {
         mark = "O";
+        color = "computerRed";
         this.playerMove();
       }
       var newSquares = this.get('board').slice(0);
-      newSquares[position] = mark;
+      newSquares[position] = [mark, color];
       this.set('board', newSquares);
     }
   },
--- app/views/BoardView.js
+++ app/views/BoardView.js
@@ -24,13 +24,19 @@ var BoardView = Backbone.View.extend({
   render: function(){
     var board = this.model.get('board');
     var html ="<tr class='row-a'>" +
-                "<td data-position='0' class='a0 col0'>" + board[0] + "</td><td data-position='1' class='a1 col1'>" + board[1] + "</td><td data-position='2' class='a2 col2'>" + board[2] + "</td>" +
+                "<td data-position='0' class='a0 col0 "+board[0][1]+"'>" + board[0][0] + "</td>" +
+                "<td data-position='1' class='a1 col1 "+board[1][1]+"'>" + board[1][0] + "</td>" +
+                "<td data-position='2' class='a2 col2 "+board[2][1]+"'>" + board[2][0] + "</td>" +
               "</tr>" +
               "<tr class='row-b'>" +
-                "<td data-position='3' class='b0 col0'>" + board[3] + "</td><td data-position='4' class='b1 col1'>" + board[4] + "</td><td data-position='5' class='b2 col2'>" + board[5] + "</td>" +
+                "<td data-position='3' class='b0 col0 "+board[3][1]+"'>" + board[3][0] + "</td>" +
+                "<td data-position='4' class='b1 col1 "+board[4][1]+"'>" + board[4][0] + "</td>" +
+                "<td data-position='5' class='b2 col2 "+board[5][1]+"'>" + board[5][0] + "</td>" +
               "</tr>" +
               "<tr class='row-c'>" +
-                "<td data-position='6' class='c0 col0'>" + board[6] + "</td><td data-position='7' class='c1 col1'>" + board[7] + "</td><td data-position='8' class='c2 col2'>" + board[8] + "</td>" +
+                "<td data-position='6' class='c0 col0 "+board[6][1]+"'>" + board[6][0] + "</td>" +
+                "<td data-position='7' class='c1 col1 "+board[7][1]+"'>" + board[7][0] + "</td>" +
+                "<td data-position='8' class='c2 col2 "+board[8][1]+"'>" + board[8][0] + "</td>" +
               "</tr>";
     this.$el.html(html);
     return this;
