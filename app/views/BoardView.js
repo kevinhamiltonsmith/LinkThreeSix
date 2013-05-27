610cdac692e4aff421446ecc1ee92f8182c66c56
Kevin Smith <kevinhsmitty@gmail.com>
2013-05-27 10:53:56 -0700

    refactored html for BoardView render function

--- app/views/BoardView.js
+++ app/views/BoardView.js
@@ -22,22 +22,16 @@ var BoardView = Backbone.View.extend({
 
   render: function(){
     var board = this.model.get('board');
-    //TODO: refactor this html -> for (var i = 0; i < 9; i++) {}
-    var html ="<tr class='row-a'>" +
-                "<td data-position='0' class='a0 col0 "+board[0][1]+"'>" + board[0][0] + "</td>" +
-                "<td data-position='1' class='a1 col1 "+board[1][1]+"'>" + board[1][0] + "</td>" +
-                "<td data-position='2' class='a2 col2 "+board[2][1]+"'>" + board[2][0] + "</td>" +
-              "</tr>" +
-              "<tr class='row-b'>" +
-                "<td data-position='3' class='b0 col0 "+board[3][1]+"'>" + board[3][0] + "</td>" +
-                "<td data-position='4' class='b1 col1 "+board[4][1]+"'>" + board[4][0] + "</td>" +
-                "<td data-position='5' class='b2 col2 "+board[5][1]+"'>" + board[5][0] + "</td>" +
-              "</tr>" +
-              "<tr class='row-c'>" +
-                "<td data-position='6' class='c0 col0 "+board[6][1]+"'>" + board[6][0] + "</td>" +
-                "<td data-position='7' class='c1 col1 "+board[7][1]+"'>" + board[7][0] + "</td>" +
-                "<td data-position='8' class='c2 col2 "+board[8][1]+"'>" + board[8][0] + "</td>" +
-              "</tr>";
+    var html = [];
+    var n = 0;
+    for (var i = 0; i < 3; i++) {
+      html.push("<tr class='row-"+i+"'>")
+      for (var j = 0; j < 3; j++){
+        html[i] = html[i] + "<td data-position='"+n+"' class='col"+j+" "+board[n][1]+"'>" + board[n][0] + "</td>";
+        n++;
+      }
+      html[i] = html[i] + "</tr>"
+    }
     this.$el.html(html);
     return this;
   }
