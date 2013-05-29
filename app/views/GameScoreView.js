787369fced0081db6e58ba9c7c4d5785bba7f064
Kevin Smith <kevinhsmitty@gmail.com>
2013-05-29 16:24:51 -0700

    made Game Score its own view

---
+++ app/views/GameScoreView.js
@@ -0,0 +1,18 @@
+var GameScoreView = Backbone.View.extend({
+
+  className: 'gameboard',
+
+  initialize: function() {
+    this.render();
+    this.model.on('change:gameScore1 change:gameScore2', function(){
+      this.render();
+    }, this);
+  },
+
+  render: function() {
+    var html = "<h2>Game Score</h2>" +
+                "<div class='player-score'>Player 1: <span>" + this.model.get('gameScore1') + "</span></div>" +
+                "<div class='computer-score'>Player 2: <span>" + this.model.get('gameScore2') + "</span></div>";
+    return this.$el.html(html);
+  }
+});
--- app/views/GameView.js
+++ app/views/GameView.js
@@ -4,8 +4,9 @@ var GameView = Backbone.View.extend({
 
   initialize: function(){
     this.boardView = new BoardView({model: this.model.get('board')});
+    this.gameScoreView = new GameScoreView({model: game});
 
-    this.model.on('change:playerScore change:computerScore change:tieScore change:gameScore1 change:gameScore2', function(){
+    this.model.on('change:playerScore change:computerScore change:tieScore', function(){
       this.render();
     }, this);
 
@@ -25,21 +26,18 @@ var GameView = Backbone.View.extend({
 //TODO: put each scoreboard in a separate view
   render: function(){
     var html = "<h1>Connect Three</h1>" +
-                  "<div class='scoreboard'>" +
-                    "<h2>Total Wins</h2>" +
-                    "<div class='player-score'>Player 1: <span>" + this.model.get('playerScore') + "</span></div>" +
-                    "<div class='computer-score'>Player 2: <span>" + this.model.get('computerScore') + "</span></div>" +
-                    "<div class='tie-score'>Tie: <span>" + this.model.get('tieScore') + "</span></div>" +
-                  "</div>" + 
-                  "<div class='gameboard'>" +
-                    "<h2>Game Score</h2>" +
-                    "<div class='player-score'>Player 1: <span>" + this.model.get('gameScore1') + "</span></div>" +
-                    "<div class='computer-score'>Player 2: <span>" + this.model.get('gameScore2') + "</span></div>" +
-                  "</div>" +
-                  "<div class='clear'></div>";
+                "<div class='scoreboard'>" +
+                  "<h2>Total Wins</h2>" +
+                  "<div class='player-score'>Player 1: <span>" + this.model.get('playerScore') + "</span></div>" +
+                  "<div class='computer-score'>Player 2: <span>" + this.model.get('computerScore') + "</span></div>" +
+                  "<div class='tie-score'>Tie: <span>" + this.model.get('tieScore') + "</span></div>" +
+                "</div>";
+    var clear = "<div class='clear'></div>";
     return this.$el.html([
       html,
-      this.boardView.$el
+      this.gameScoreView.$el,
+      clear,
+      this.boardView.$el,
     ]);
   }
 });
