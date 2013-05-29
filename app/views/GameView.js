b7550f79809d83813609f019e73d430884ad7cd8
Kevin Smith <kevinhsmitty@gmail.com>
2013-05-28 20:54:16 -0700

    added game score box and styled

--- app/views/GameView.js
+++ app/views/GameView.js
@@ -24,13 +24,18 @@ var GameView = Backbone.View.extend({
   },
 
   render: function(){
-    var html = "<h1>Backbone Tic-Tac-Toe</h1>" +
+    var html = "<h1>Connect Three</h1>" +
                   "<div class='scoreboard'>" +
-                    "<h2>Wins</h2>" +
+                    "<h2>Total Wins</h2>" +
                     "<div class='player-score'>Player 1: <span>" + this.model.get('playerScore') + "</span></div>" +
                     "<div class='computer-score'>Player 2: <span>" + this.model.get('computerScore') + "</span></div>" +
                     "<div class='tie-score'>Tie: <span>" + this.model.get('tieScore') + "</span></div>" +
-                  "</div>";
+                  "</div>" + 
+                  "<div class='gameboard'>" +
+                    "<h2>Game Score</h2>" +
+                    "<div class='player-score'>Player 1: <span>" + this.model.get('playerScore') + "</span></div>" +
+                    "<div class='computer-score'>Player 2: <span>" + this.model.get('computerScore') + "</span></div>" +
+                  "</div><div style='clear: both;'></div>";
     return this.$el.html([
       html,
       this.boardView.$el
