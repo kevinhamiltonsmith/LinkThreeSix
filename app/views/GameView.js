3800b7e580381c4bfcb3d200b10bf0798b989db9
Kevin Smith <kevinhsmitty@gmail.com>
2013-07-01 23:21:30 -0700

    added about tab text, bug fix on nav event binding

--- app/views/GameView.js
+++ app/views/GameView.js
@@ -4,7 +4,9 @@ var GameView = Backbone.View.extend({
 
   events: {
     'click .new-game-button, .new-game-button-nav': 'newGame',
-    'click .new-game-button, .new-game-button-nav, .close-button': 'closeEndGame'
+    'click .new-game-button, .new-game-button-nav, .close-button': 'closeEndGame',
+    'click .how-to-play': 'howToPlayEvent',
+    'click .about-nav': 'aboutEvent'
   },
 
   initialize: function(){
@@ -23,6 +25,7 @@ var GameView = Backbone.View.extend({
       this.boardView = new BoardView({model: this.model.get('board')});
       this.model.boardChangeListener();
       this.render();
+      Gumby.initialize('switches');
     }, this);
   },
 
@@ -37,8 +40,8 @@ var GameView = Backbone.View.extend({
           '<div class="logged-in-nav">' +
             '<ul class="eight columns ul-nav">' +
               '<li class="new-game-button-nav"><a href="#">New Game</a></li>' +
-              '<li><a href="#" class="switch" gumby-trigger="#howToPlay">How to Play</a></li>' +
-              '<li><a href="#">About</a></li>' +
+              '<li><a href="#" class="switch how-to-play" gumby-trigger="#howToPlay">How to Play</a></li>' +
+              '<li><a href="#" class="switch about-nav" gumby-trigger="#about">About</a></li>' +
             '</ul>' +
           '</div>' +
         '</div>' +
@@ -60,9 +63,24 @@ var GameView = Backbone.View.extend({
                 '<p>6 in a row: 20 points</p>' +
               '</div>' +
               '<p>The game is scored in real time, after every move. The Game Score board highlights the current game leader.</p>' +
-              '<p>After each game, the Total Wins board updates to reflect the winner of the current game, and keeps track of the entire match.</p>' +
+              '<p>After each game, the Total Wins board updates to reflectgit  the winner of the current game, and keeps track of the entire match.</p>' +
               '<p><strong>The first player to win 3 single games, wins the match!</strong></p>' +
               '<div class="btn primary medium lets-play"><a href="#" class="switch" gumby-trigger="|#howToPlay">Let\'s Play!</a></div>' +
+            '</div>' +
+          '</div>' +
+        '</div>' +
+      '</div>' +
+      '<div class="modal" id="about">' +
+        '<div class="content">' +
+          '<a class="close switch close-button" gumby-trigger="|#about"><i class="icon-cancel-circled" /></i></a>' +
+          '<div class="row">' +
+            '<div class="ten columns centered center-text">' +
+              '<h3>About</h3>' +
+              '<h4>Created by: <a href="http://kevinhamiltonsmith.com">Kevin Smith</a> at <a href="http://hackreactor.com">Hack Reactor</a></h4>' +
+              '<p>This game started as a one-day project I created at Hack Reactor to learn the basics of a JavaScript MVC framework called Backbone.js.</p>' +
+              '<p>It was originally built as traditional tic-tac-toe game, but this was fairly simple to accomplish, and I wast left wanting to build a game that I could play with my friends.</p>' +
+              '<p>My next goal for this game is to implement a computer AI opponent with varying difficulty levels.</p>' +
+              '<p>If you enjoy Link Thirty Six, feel free to fork or star on GitHub!</p>' +
             '</div>' +
           '</div>' +
         '</div>' +
@@ -83,7 +101,18 @@ var GameView = Backbone.View.extend({
     this.model.newBoard();
   },
 
-  closeEndGame: function() {
+  closeEndGame: function(e) {
+    e.preventDefault();
     $('.end-game').fadeOut('fast');
+  },
+
+  howToPlayEvent: function(e) {
+    e.preventDefault();
+    $('.how-to-play').trigger('gumby.trigger');
+  },
+
+  aboutEvent: function(e) {
+    e.preventDefault();
+    $('.about-nav').trigger('gumby.trigger');
   }
 });
