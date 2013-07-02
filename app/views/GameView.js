b85280fffd9175769ce5e3e87ecec19be64f410a
Kevin Smith <kevinhsmitty@gmail.com>
2013-07-01 22:55:02 -0700

    added game rules text and formatting

--- app/views/GameView.js
+++ app/views/GameView.js
@@ -37,9 +37,33 @@ var GameView = Backbone.View.extend({
           '<div class="logged-in-nav">' +
             '<ul class="eight columns ul-nav">' +
               '<li class="new-game-button-nav"><a href="#">New Game</a></li>' +
-              '<li><a href="#">How to Play</a></li>' +
+              '<li><a href="#" class="switch" gumby-trigger="#howToPlay">How to Play</a></li>' +
               '<li><a href="#">About</a></li>' +
             '</ul>' +
+          '</div>' +
+        '</div>' +
+      '</div>' +
+      '<div class="modal" id="howToPlay">' +
+        '<div class="content">' +
+          '<a class="close switch close-button" gumby-trigger="|#howToPlay"><i class="icon-cancel-circled" /></i></a>' +
+          '<div class="row">' +
+            '<div class="ten columns centered center-text">' +
+              '<h3>How to Play Link Thirty Six</h3>' +
+              '<p>This is a two player game. Alternating turns, each player places their mark on a single board square, until the board is filled</p>' +
+              '<p class="info-center"><span class="info-blue">Player 1: Blue X\'s</span><span class="info-red">Player 2: Red O\'s</span></p>' +
+              '<p>Your objective is to make links of <strong>3 in a row, or more</strong>. Links can be made horizontally, vertically, or diagonally.</p>' +
+              '<div class="link-scoring">' +
+                '<p>Link Scoring:</p>' +
+                '<p>3 in a row: 1 point</p>' +
+                '<p>4 in a row: 3 points</p>' +
+                '<p>5 in a row: 10 points</p>' +
+                '<p>6 in a row: 20 points</p>' +
+              '</div>' +
+              '<p>The game is scored in real time, after every move. The Game Score board highlights the current game leader.</p>' +
+              '<p>After each game, the Total Wins board updates to reflect the winner of the current game, and keeps track of the entire match.</p>' +
+              '<p><strong>The first player to win 3 single games, wins the match!</strong></p>' +
+              '<div class="btn primary medium lets-play"><a href="#" class="switch" gumby-trigger="|#howToPlay">Let\'s Play!</a></div>' +
+            '</div>' +
           '</div>' +
         '</div>' +
       '</div>';
